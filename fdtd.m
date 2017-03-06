1d116fd2b3ecf5d2b1d1b30f5585463918e947dd
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2017-03-06 20:13:13 +0100

    Testing

--- fdtd.m
+++ fdtd.m
@@ -5,7 +5,7 @@ constants; % Loads constants
 
 %% Problem definition.
 x = (0:0.05:10)';
-finalTime = 1000e-9;
+finalTime = 100e-9;
 
 % Materials.
 
@@ -25,7 +25,7 @@ initialEz = exp(- (x-5).^2);
 cells = size(x,1);
 
 ez=zeros(size(x,1),2);
-hy=zeros(size(x,1),2);
+hy=zeros(size(x,1)-1,2);
 if (exist('initialEz','var'))
     ez(:,1) = initialEz(:);
 end
@@ -44,28 +44,36 @@ dt = cfl*dx/c0;
 cE = dt/eps0/dx;
 cH = dt/mu0/dx;
 
+mur = zeros(2,1);
+
 %% Performs time integration.
 tic;
 for t=0:dt:finalTime
     % --- Updates E field ---
-    for i=2:cells
+    for i=2:(cells-1)
         ez(i,2)=ez(i,1)+cE*(hy(i-1,1)-hy(i,1));
     end
-%     ez(2:end,2) = ez(2:end,1) + cE.* (hy(1:(end-1),1)-hy(2:end,1));
+%     ez(2:end-1,2) = ez(2:end-1,1) + cE.* (hy(1:end-2,1)-hy(2:end-1,1));
     
     % --- Sources ---
 %     ez(excPoint,2) = ez(excPoint,2) + exp(- 0.5*((t-delay)/spread)^2);
     
     % --- Boundary conditions ---
-    ez(    1, 2) = -ez(      2, 2);
-    ez(cells, 2) = -ez(cells-1, 2); % PEC
+%     ez(    1, 2) = 0;
+%     ez(cells, 2) = 0; % PEC
+%     ez(1,2)    =ez(    1,1) - 2*cE*hy(      1,1);
+%     ez(cells,2)=ez(cells,1) + 2*cE*hy(cells-1,1); % PMC
+    ez(1,2) = mur(1);
+    mur(1)  = ez(2,2);
+    ez(cells,2) = mur(2);
+    mur(2)  = ez(cells-1,2);
     
     % --- Updates H field ---
     for i=1:cells-1
         hy(i,2)=hy(i,1)+cH*(ez(i,2)-ez(i+1,2));
     end
 %     hy(1:(end-1),2) = hy(1:(end-1),2) + cH.* (ez(1:(end-1),2) - ez(2:end,2));
-    
+
     ez(:,1)=ez(:,2);
     hy(:,1)=hy(:,2);
     
@@ -81,7 +89,7 @@ for t=0:dt:finalTime
     plot(hy(:,2));
     hold on;
     axis([0 cells -0.005 0.005]);
-    
+%     pause(.01);
     drawnow;
 end
 toc;
