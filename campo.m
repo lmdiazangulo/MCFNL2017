90320d4cb14ba1b17e7a32feb7801bca7ccba8c5
Jesús <Jess>
2017-03-14 13:46:45 +0100

    patata2

--- campo.m
+++ campo.m
@@ -5,14 +5,14 @@ clc;
 % Definimos los parámetros
 
 s=0.1;
-x0=0.5;
+L=0.5;
 E0=1;
 x=0.95;
 t=0:1e-12:100e-9;
 
 % El campo es
 
-Campo=analyticalGaussianRight(E0,x0,s,x,t);
+Campo=analyticalGaussianRight(E0,L,s,x,t);
 
 % Hacemos el plot
 
